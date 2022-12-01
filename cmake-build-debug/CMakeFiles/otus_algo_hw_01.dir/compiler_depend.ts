af1728210d22d258d3f1fc3634414fc4abf3286c
free0ON <8333184@gmail.com>
2022-12-02 02:56:39 +0300

    otus_algo_hw_01

---
+++ cmake-build-debug/CMakeFiles/otus_algo_hw_01.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for otus_algo_hw_01.
---
+++ cmake-build-debug/CMakeFiles/otus_algo_hw_02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/otus_algo_hw_02.dir/otus_algo_hw_02.cpp.o"
+  "CMakeFiles/otus_algo_hw_02.dir/otus_algo_hw_02.cpp.o.d"
+  "otus_algo_hw_02"
+  "otus_algo_hw_02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/otus_algo_hw_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
