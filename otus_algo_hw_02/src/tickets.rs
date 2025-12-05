c0071b19d4f6edd68fe1e0c46330075a2cca6063
free0ON <8333184@gmail.com>
2025-12-05 15:51:29 +0300

    rust update

--- otus_algo_hw_02/src/tickets.rs
+++ otus_algo_hw_02/src/tickets.rs
@@ -1,16 +1,14 @@
-fn get_sum_number(_number: u64, n: u32) -> u64
-    {
-        let mut sum: u64 = 0;
-        let mut number: u64 = _number;
-        for _i in 0..=n
-        {
-            sum += number % 10;
-            number /= 10;
-        }
+fn get_sum_number(mut number: u64 ) -> usize {
+	let mut sum: u64 = 0;
+	//let mut number: u64 = _number;
+	while number > 0
+	{
+		sum += number % 10;
+		number /= 10;
+	}
 
-        return sum;
-    }
-
+	return sum as usize;
+}
 pub fn brut_get_happy_count(n: u32) -> u64
 {
         let mut count: u64 = 0;
@@ -19,7 +17,7 @@ pub fn brut_get_happy_count(n: u32) -> u64
         {
             for right in 0..max_num
             {
-                if get_sum_number(left, n) == get_sum_number(right, n)  
+                if get_sum_number(left) == get_sum_number(right)  
                 {
                     count += 1;
                 }
@@ -33,19 +31,11 @@ pub fn fast_get_happy_count(n: u32) -> u64
     let mut count: u64 = 0;
     let max_num: u64 = u64::pow(10,n);
     let sums_number: usize = (9*n + 1) as usize;
-    let mut sums: Vec<u64> = Vec::new();
-    sums.resize(sums_number, 0);
-
-    for number in 0..max_num
-    {
-        let index = get_sum_number(number, n) as usize;
-        sums[index] += 1; 
-    }
-
-    for i in 0..sums_number
-    {
-        count += sums[i] * sums[i]
-    }
-
-    return count;
+    let mut sums: Vec<u64> = vec![0;sums_number];
+	(0..max_num).for_each(|number| {
+		sums[get_sum_number(number)] += 1;
+	});
+	sums.iter().for_each(|x| count += x*x);
+    
+	return count;
 }
